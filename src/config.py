edb26e4300a15aaf275d66782a3ca7b55c42b0b1
Polina Belaya <polinabeline@gmail.com>
2025-08-13 17:20:34 +0300

    poetry add alembic + config db

--- src/config.py
+++ src/config.py
@@ -37,14 +37,14 @@ class Settings(BaseSettings):
     DB_NAME: str
 
     @property
-    def DATABASE_URL_asyncpg(self):
+    def db_url(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    @property
-    def DATABASE_URL_psycopg(self):
-        return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    # @property
+    # def DATABASE_URL_psycopg(self):
+    #     return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    model_config = model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
